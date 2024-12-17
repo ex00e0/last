dfcefe861796c39d16f91e74afb361811cbb1ed5
trapbed <hasanova.leya@yandex.ru>
2024-12-17 23:14:59 +0500

    add back (need check)

--- app/Http/Controllers/PageController.php
+++ app/Http/Controllers/PageController.php
@@ -4,7 +4,9 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
-// use Illuminate\Support\Facades\Validator;
+use App\Models\Event;
+use App\Models\News;
+use Illuminate\Support\Facades\Validator;
 
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Hash;
@@ -12,14 +14,15 @@ use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\DB;
 
 use App\Models\User;
+use SebastianBergmann\CodeCoverage\Report\Xml\Project;
 
 class PageController extends Controller
 {
-    //
-    public function login () {
+    public function login_show () {
         return view('login');
     }
-    public function login_db (Request $request) {
+    //LOGIN
+    public function login (Request $request) {
        $check = User::where('login','=', $request->login)->exists();
        if(strlen(strip_tags(trim($request->password))) != 0 && strlen(strip_tags(trim($request->login))) != 0){
             if($check == true){
@@ -32,13 +35,13 @@ class PageController extends Controller
                 if (Hash::check($request->password, $password)) {
                     Auth::login( User::find($id));
                     if ($role == 0) {
-                        return redirect()->route('login')->withErrors(['message'=>'Вы вошли в профиль!']);
+                        return redirect()->route('index')->withErrors(['message'=>'Вы вошли в профиль!']);
                     }
                     else if ($role == 1) {
                         return redirect()->route('admin/index')->withErrors(['message'=>'Вы вошли в профиль как админ!']);
                     }
                 } else {
-                    return back()->withErrors(['message'=>'Неверные данные!'])->withInput();
+                    return back()->withErrors(['message'=>'Неверный пароль!'])->withInput();
             }
            }
            else{
@@ -50,5 +53,301 @@ class PageController extends Controller
        }
        
     }
+// ALL EVENTS ON GENERAL PAGE
+    public function get_all_events($id=null){
+        $one_event = null;
+        if($id){
+            $one_event = Event::select('*')->where('id', '=', $id)->get()[0];
+        }
+        $events = Event::select('*')->limit(1)->get();
+        $count = Event::select('*')->get()->count();
+        return view('events', ['events'=>$events, 'count'=>$count, 'one_event'=>$one_event, 'event_id'=>$id]);
+    }
+// LIMIT 3 EVENTS (INDEX)
+    public function limit_3(){
+        $soon = Event::select('*')->limit(3)->orderBy('date', 'ASC')->get();
+        return view('limit_3', ['events'=>$soon]);
+    }
+// LIMIT 4 NEWS (INDEX)
+    public function limit_4($news_id = null){
+        $one_news = null;
+        if($news_id){
+            $one_news = News::select('*')->where('id', '=', $news_id)->get()[0];
+        }
+        // ADD LIMIT 4
+        $news = News::select('*')->orderBy('created_at')->get();
 
+        $count = $news->count();
+        return view('news', ['news'=>$news, 'count'=>$count, 'one_news'=>$one_news, 'news_id'=>$news_id]);
+    }
+// ADMIN CREATE EVENT
+    public function create_event(Request $request){
+        $data = [
+            'title'=>$request->title,
+            'subtitle'=>$request->subtitle,
+            'age'=>$request->age,
+            'long'=>$request->long,
+            'date'=>$request->date,
+            'description'=>$request->description,
+            'squad'=>$request->squad,
+            'image'=>$request->image
+        ];
+        $rules = [
+            'title'=>'required|unique:events',
+            'subtitle'=>'required',
+            'age'=>'required',
+            'long'=>'required',
+            'date'=>'required',
+            'description'=>'required',
+            'squad'=>'required',
+            'image'=>'required|mimes:jpeg,png,jpg'
+        ];
+        $messages = [
+            'title.required'=>'Заполните заголовок',
+            'title.unique'=>'Заголовок должен быть уникальным',
+            'subtitle.required'=>'Заполните подзаголовок',
+            'age.required'=>'Выберите возраст',
+            'long.required'=>'Выберите продолжительность мероприятия в минутах',
+            'date.required'=>'Выберите дату мероприятия',
+            'description.required'=>'Заполните описание',
+            'squad.required'=>'Заполните команду',
+            'image.required'=>'Выберите изображение',
+            'image.mimes'=>'Тип файла должен быть изображением'];
+        $validate = Validator::make($data, $rules, $messages);
+        if($validate->fails()){
+            return redirect('/')
+            ->withErrors($validate)
+            ->withInput();
+        }
+        else{
+            $create = Event::insert([
+                'title'=>$request->title,
+                'subtitle'=>$request->subtitle,
+                'age'=>$request->age,
+                'long'=>$request->long,
+                'date'=>$request->date,
+                'description'=>$request->description,
+                'squad'=>$request->squad,
+                'image'=>$request->file('image')->getClientOriginalName()
+            ]);
+            $events = Event::select('*')->limit(1)->get();
+            $count = Event::select('*')->get()->count();
+            if($create){
+                $image_i = $request->file('image')->getClientOriginalName();
+                $request->file('image')->move(public_path() . "/img/", $image_i);
+                return view('events', ['events'=>$events, 'count'=>$count])->withErrors(['message'=>'Успешное добавление мероприятия!']);
+            }
+            else{
+                return view('events', ['events'=>$events, 'count'=>$count])->withErrors(['message'=>'Не удалось добавить мероприятие!']);
+            }
+        }
+    }
+// ADMIN CREATE NEWS
+    public function create_news(Request $request){
+        $data = [
+            'title'=>$request->title,
+            'description'=>$request->description,
+            'image'=>$request->image
+        ];
+        $rules = [
+            'title'=>'required|unique:news',
+            'description'=>'required',
+            'image'=>'required|mimes:jpeg,jpg,png'
+        ];
+        $messages = [
+            'title.required'=>'Заполните заголовок',
+            'title.unique'=>'Заголовок должен быть уникальным',
+            'description.required'=>'Заполните описание',
+            'image.required'=>'Выберите изображение', 
+            'image.mimes'=>'Формат файла должен быть jpeg, jpg или png'];
+        $validate = Validator::make($data, $rules, $messages);
+        if($validate->fails()){
+            return redirect('news')
+            ->withErrors($validate)
+            ->withInput();
+        }
+        else{
+            // dd($request->image);
+            $create_news = News::insert([
+                'title'=>$request->title,
+                'description'=>$request->description, 
+                'image'=>$request->file('image')->getClientOriginalName()
+            ]); 
+            
+            $news = News::select('*')->get();
+            $count = News::select('*')->get()->count();
+            if($create_news){
+                $image_i = $request->file('image')->getClientOriginalName();
+                $request->file('image')->move(public_path() . "/img/", $image_i);
+                return view('news', ['count'=>$count, 'news'=>$news])->withErrors(['message'=>'Успешное создание новости']);
+            }
+            else{
+                return view('news', ['count'=>$count, 'news'=>$news])->withErrors(['message'=>'Не удалось создать новость'])->withInputs();
+            }
+            // return view('news', ['image'=>$request->image]);
+        }
+    }
+
+
+// ADMIN UPDATE EVENT
+    public function update_event(Request $request){
+        // dd($request->image);
+        $data = [
+            'title'=>$request->title,
+            'subtitle'=>$request->subtitle,
+            'age'=>$request->age,
+            'long'=>$request->long,
+            'date'=>$request->date,
+            'description'=>$request->description,
+            'squad'=>$request->squad
+        ];
+        $rules = [
+            'title'=>'required',
+            'subtitle'=>'required',
+            'age'=>'required',
+            'long'=>'required',
+            'date'=>'required',
+            'description'=>'required',
+            'squad'=>'required'
+        ];
+        $messages = [
+            'title.required'=>'Заполните заголовок',
+            'subtitle.required'=>'Заполните подзаголовок',
+            'age.required'=>'Выберите возраст',
+            'long.required'=>'Выберите продолжительность мероприятия в минутах',
+            'date.required'=>'Выберите дату мероприятия',
+            'description.required'=>'Заполните описание',
+            'squad.required'=>'Заполните команду'
+        ];
+        if($request->image){
+            // array_push($data, );
+            $data['image'] = $request->image;
+            $rules['image'] = 'mimes:jpg,jpeg,png';
+            $messages['image.mimes'] = 'Выбранный файл не изображение!';
+        }
+        $validate = Validator::make($data, $rules, $messages);
+        if($validate->fails()){
+            return redirect('/')
+            ->withErrors($validate)
+            ->withInput();
+        }
+        else{
+            $update = Event::where('id', '=', $request->id)->update([
+                'title'=>$request->title,
+                'subtitle'=>$request->subtitle,
+                'age'=>$request->age,
+                'long'=>$request->long,
+                'date'=>$request->date,
+                'description'=>$request->description,
+                'squad'=>$request->squad
+            ]);
+            if($request->image){
+                $update = Event::where('id','=', $request->id)->update([
+                    'image'=>$request->file('image')->getClientOriginalName()
+                ]);
+            }
+            $events = Event::select('*')->limit(1)->get();
+            $count = Event::select('*')->get()->count();
+            if($update){
+                if($request->image){
+                    $image_i = $request->file('image')->getClientOriginalName();
+                    $request->file('image')->move(public_path() . "/img/", $image_i);
+                }
+                return view('events', ['count'=>$count, 'events'=>$events])->withErrors(['message'=>'Успешное обновление мероприятия']);
+            }
+            else{
+                return view('events', ['events'=>$events, 'count'=>$count])->withErrors(['message'=>'Не удалось добавить мероприятие!']);
+            }
+        }
+    }
+
+// ADMIN UPDATE NEWS
+    public function update_news(Request $request){
+        $data = [
+            'title'=>$request->title,
+            'description'=>$request->description
+        ];
+        $rules = [
+            'title'=>'required',
+            'description'=>'required'
+        ];
+        $messages = [
+            'title.required'=>'Заполните заголовок',
+            'description.required'=>'Заполните описание'
+        ];
+        if($request->image){
+            // array_push($data, );
+            $data['image'] = $request->image;
+            $rules['image'] = 'mimes:jpg, jpeg, png';
+            $messages['image.required'] = 'Выбранный файл не изображение!';
+        }
+        $validate = Validator::make($data, $rules, $messages);
+        if($validate->fails()){
+            return redirect('news')
+            ->withErrors($validate)
+            ->withInput();
+        }
+        else{
+            // dd($request->image);
+            $update_news = News::where('id', '=', $request->id)->update([
+                'title'=>$request->title,
+                'description'=>$request->description,
+            ]); 
+            if($request->image){
+                $update_news = News::where('id','=', $request->id)->update([
+                    'image'=>$request->file('image')->getClientOriginalName()
+                ]);
+            }
+            
+            $news = News::select('*')->get();
+            $count = News::select('*')->get()->count();
+            if($update_news){
+                if($request->image){
+                    $image_i = $request->file('image')->getClientOriginalName();
+                    $request->file('image')->move(public_path() . "/img/", $image_i);
+                }
+                return view('news', ['count'=>$count, 'news'=>$news])->withErrors(['message'=>'Успешное обновление новости']);
+            }
+            else{
+                return view('news', ['count'=>$count, 'news'=>$news])->withErrors(['message'=>'Не удалось обновить новость'])->withInputs();
+            }
+        }
+            // return view('news', ['image'=>$request->image]);
+    }
+// DELETE EVENT ADMIN
+    public function delete_event($id){
+        $delete = Event::where('id', '=', $id)->delete();
+        $events = Event::select('*')->limit(1)->get();
+        $count = Event::select('*')->get()->count();
+        if($delete){
+            return view('events', ['count'=>$count, 'events'=>$events])->withErrors(['message'=>'Успешное удаление мероприятия']);
+        }
+        else{
+            return view('events', ['count'=>$count, 'events'=>$events])->withErrors(['message'=>'Не удалось удалить мероприятие']);
+        }
+    }
+// DELETE NEWS ADMIN
+    public function news_d($id){
+        $delete = News::where('id', '=', $id)->delete();
+        $events = News::select('*')->limit(1)->get();
+        $count = News::select('*')->get()->count();
+        if($delete){
+            return view('news', ['count'=>$count, 'events'=>$events])->withErrors(['message'=>'Успешное удаление новости']);
+        }
+        else{
+            return view('news', ['count'=>$count, 'events'=>$events])->withErrors(['message'=>'Не удалось удалить новость']);
+        }
+    }
+
+
+    // MORE_INFO
+    public function news_more($id){
+        $one_news = News::select('*')->where('id', '=', $id)->get();
+        return view('one_news', ['info'=>$one_news]);
+    }
+
+    public function event_more($id){
+        $one_event = Event::select('*')->where('id', '=', $id)->get();
+        return view('one_news', ['info'=>$one_event]);
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,41 @@ Route::get('/one_new', function(){return view('one_new');})->name('one_new');
 
 Route::get('/login', [PageController::class, 'login'])->name('login');
 Route::post('/login_db', [PageController::class, 'login_db'])->name('login_db');
+
+// LOGIN
+Route::post('/login', [PageController::class, 'login'])->name('login');
+
+
+// AFISHA
+Route::get('/', [PageController::class, 'get_all_events']);
+// INDEX LIMIT 3 EVENT 
+Route::get('/limit_3', [PageController::class , 'limit_3'])->name('limit_3');
+// INDEX NEWS LIMIT 4
+Route::get('/news', [PageController::class, 'limit_4'])->name('news');
+
+
+// ADMIN
+
+// ROUTE LINK ONE EVENT TO UPDATE
+Route::get('/events/{id?}', [PageController::class, 'get_all_events'])->name('index');
+// POST QUERY CREATE EVENT
+Route::post('/create_event', [PageController::class, 'create_event'])->name('create_event');
+// POST QUERY UPDATE EVENT
+Route::post('/update_event', [PageController::class, 'update_event'])->name('update_event');
+// ADMIN DELETE EVENT
+Route::get('/delete_event/{id}', [PageController::class, 'delete_event'])->name('delete_event');
+
+
+// POST CRAETE NEWS
+Route::post('/create_news', [PageController::class, 'create_news'])->name('create_news');
+// UPDATE GET VIEW
+Route::get('/news/{id?}', [PageController::class, 'limit_4'])->name('news_u');
+// UPDATE POST UPDATE NEWS
+Route::post('/update_news', [PageController::class, 'update_news'])->name('update_news');
+// DELETE NEWS ADMIN 
+Route::get('/news_d/{id}', [PageController::class, 'news_d'])->name('news_d');
+
+
+// MORE INFO
+Route::get('/news_more/{id}', [PageController::class, 'news_more'])->name('news_m');
+Route::get('/event_more/{id}', [PageController::class, 'event_more'])->name('event_more');
