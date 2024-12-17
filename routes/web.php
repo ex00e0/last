4e6c358f4b6dc1fae4efaa98e9c514d16981908f
trapbed <hasanova.leya@yandex.ru>
2024-12-17 22:14:29 +0500

    page one_new (done)

--- routes/web.php
+++ routes/web.php
@@ -9,6 +9,7 @@ use Illuminate\Support\Facades\DB;
 Route::get('/', function(){return view('index');})->name('index');
 Route::get('/all_events', function(){return view('all_events');})->name('all_events');
 Route::get('/all_news', function(){return view('all_news');})->name('all_news');
+Route::get('/one_new', function(){return view('one_new');})->name('one_new');
 
 
 Route::get('/login', [PageController::class, 'login'])->name('login');
