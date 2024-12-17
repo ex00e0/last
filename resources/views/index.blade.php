edd43ee3365d2c3057314a874e8c8f2b9696b280
trapbed <hasanova.leya@yandex.ru>
2024-12-17 20:22:49 +0500

    main (done)

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -63,5 +63,36 @@
         <div>Коллектив театра</div>
     </div>
 </div>
+<div class="news">
+    <div class="headline c2 r1">Новости театра</div>
+    <div class="show_all c3 r1">Показать все</div>
+    <img src="{{asset('images/Arrow 1.svg')}}" class="show_all_arrow c4 r1">
+    <div class="c2-5 r2 block_news">
+        <div>
+            <img class="c1 r1 new_img" src="{{asset('images/Rectangle 153.jpg')}}">
+            <div class="c1 r3 new_name">Академия вокального искусства Аскара Абдразакова</div>
+            <div class="c1 r3 new_date">09.12.2022</div>
+            <div class="c1 r4 new_desc">Завершилась первая сессия проекта «Академия вокального искусства Аскара Абдразакова». Слушателей ждали четыре насыщенных знаниями и эмоциями дня.</div>
+        </div>
+        <div>
+            <img class="c1 r1 new_img" src="{{asset('images/Rectangle 153.jpg')}}">
+            <div class="c1 r3 new_name">Академия вокального искусства Аскара Абдразакова</div>
+            <div class="c1 r3 new_date">09.12.2022</div>
+            <div class="c1 r4 new_desc">Завершилась первая сессия проекта «Академия вокального искусства Аскара Абдразакова». Слушателей ждали четыре насыщенных знаниями и эмоциями дня.</div>
+        </div>
+        <div>
+            <img class="c1 r1 new_img" src="{{asset('images/Rectangle 153.jpg')}}">
+            <div class="c1 r3 new_name">Академия вокального искусства Аскара Абдразакова</div>
+            <div class="c1 r3 new_date">09.12.2022</div>
+            <div class="c1 r4 new_desc">Завершилась первая сессия проекта «Академия вокального искусства Аскара Абдразакова». Слушателей ждали четыре насыщенных знаниями и эмоциями дня.</div>
+        </div>
+        <div>
+            <img class="c1 r1 new_img" src="{{asset('images/Rectangle 153.jpg')}}">
+            <div class="c1 r3 new_name">Академия вокального искусства Аскара Абдразакова</div>
+            <div class="c1 r3 new_date">09.12.2022</div>
+            <div class="c1 r4 new_desc">Завершилась первая сессия проекта «Академия вокального искусства Аскара Абдразакова». Слушателей ждали четыре насыщенных знаниями и эмоциями дня.</div>
+        </div>
+    </div>
+</div>
 
 @endsection
